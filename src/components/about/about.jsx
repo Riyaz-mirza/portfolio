14859df05b3705c600731991615e8580d105cba5
Riyaz Mirza <riyazmirza228@gmail.com>
2026-03-22 12:36:38 +0530

    added

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/Riyaz.jpeg'
+import ME from '../../assets/Riyaz.jpg'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA  from './CTA'
-import ME from '../../assets/new.png'
+import ME from '../../assets/Riyaz.jpg'
 import Headersocial from './Headersocial'
 
 const header = () => {
