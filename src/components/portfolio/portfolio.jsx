45b26469e1b1cf785d989e08591acd51f7e61e2e
Riyaz Mirza <riyazmirza228@gmail.com>
2026-03-22 14:52:39 +0530

    fixes

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,9 +3,7 @@ import './portfolio.css'
 import IMG1 from'../../assets/donations.png'
 import IMG2 from'../../assets/portfolio2.png'
 import IMG3 from'../../assets/portfolio3.jpg'
-import IMG4 from'../../assets/portfolio4.jpg'
-import IMG5 from'../../assets/portfolio5.png'
-import IMG6 from'../../assets/portfolio6.jpg'
+
 
 
 
